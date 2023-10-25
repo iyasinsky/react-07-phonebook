1c7fb5565fba21a3225a00b92e639ab3d168b21b
Igor Yasinsky <iyasinskyy@gmail.com>
2023-10-25 12:46:46 +0300

    fixing store

--- src/redux/Store/index.js
+++ src/redux/Store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsSlice } from 'redux/contactsSlice';
-import { filterSlice } from 'redux/filterSlice';
+import { contactsSlice } from '../contactsSlice';
+import { filterSlice } from '../filterSlice';
 
 export const store = configureStore({
   reducer: {
